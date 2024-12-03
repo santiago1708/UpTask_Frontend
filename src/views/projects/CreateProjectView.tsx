95841ed06b2959fd581b7ec5f51367ac2a9633ea
santiago1708 <vela123tique@gmail.com>
2024-12-03 18:11:34 -0500

    se conecto el front con el backend y se agregaron mas estilos al frontend

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify'
 import ProjectForm from "@/components/projects/ProjectForm";
-import { ProjectFormData } from "types";
+import { ProjectFormData } from "@/types/index";
+import { createProject } from "@/api/ProjectAPI";
 
 export default function CreateProjectView() {
 
-    const initialValues : ProjectFormData = {
+    const navigate = useNavigate();
+    const initialValues: ProjectFormData = {
         projectName: "",
         clientName: "",
         description: ""
     }
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-    const handleForm = (data : ProjectFormData) => {
-        console.log(data);
+    const handleForm = async (data: ProjectFormData) => {
+        const response = await createProject(data);
+        toast.success(response)
+        navigate("/")
     }
 
     return (
@@ -35,7 +40,7 @@ export default function CreateProjectView() {
                     onSubmit={handleSubmit(handleForm)}
                     noValidate
                 >
-                    <ProjectForm 
+                    <ProjectForm
                         register={register}
                         errors={errors}
                     />
