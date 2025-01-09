060868ced44607e7a5c89a769df03431c6692cea
santiago1708 <vela123tique@gmail.com>
2025-01-09 15:45:57 -0500

    las tareas se arrastran y se cambian de estado de forma dinamica

--- src/components/task/DropTask.tsx
+++ src/components/task/DropTask.tsx
@@ -1,8 +1,18 @@
+import { useDroppable } from '@dnd-kit/core'
 
-export default function DropTask() {
+type DropTaskProps = {
+    status: string
+}
+
+export default function DropTask({ status }: DropTaskProps) {
+
+    const { isOver, setNodeRef } = useDroppable({
+        id: status
+    })
+
     return (
-        <div 
-            
+        <div
+            ref={setNodeRef}
             className="text-xs font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5 grid place-content-center text-slate-500"
         >
             Soltar tarea aqui
--- src/components/task/TaskList.tsx
+++ src/components/task/TaskList.tsx
@@ -1,8 +1,12 @@
-import { DndContext } from '@dnd-kit/core'
-import { Task } from "@/types/index"
+import { DndContext, DragEndEvent } from '@dnd-kit/core'
+import { Task, TaskStatus } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTranslations } from "@/locales/es"
 import DropTask from "./DropTask"
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { updateStatus } from '@/api/TaskAPI'
+import { toast } from 'react-toastify'
+import { useParams } from 'react-router-dom'
 
 type TaskListProps = {
     tasks: Task[]
@@ -37,19 +41,45 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
         currentGroup = [...currentGroup, task]
         return { ...acc, [task.status]: currentGroup };
     }, initialStatusGroups);
+
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: updateStatus,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['project', projectId] })
+            toast.success(data)
+        }
+    })
+
+    const handleDragEnd = (e: DragEndEvent) => {
+        const { over, active } = e
+
+        if (over && over?.id) {
+            const taskId = active.id.toString()
+            const status = over.id as TaskStatus
+
+            mutate({projectId, taskId, status})
+        }
+    }
     return (
         <>
             <h2 className="text-5xl font-black my-10">Tareas</h2>
 
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-                <DndContext>
+                <DndContext onDragEnd={handleDragEnd}>
                     {Object.entries(groupedTasks).map(([status, tasks]) => (
                         <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
                             <h3
                                 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
                             >{statusTranslations[status]}</h3>
 
-                            <DropTask />
+                            <DropTask status={status} />
 
                             <ul className='mt-5 space-y-5'>
                                 {tasks.length === 0 ? (
